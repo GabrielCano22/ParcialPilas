85ccd6ff044f9460107f51c14875aa5462e1c4f4
Gabriel Jaime Cano Oquendo <93448629+GabrielCano22@users.noreply.github.com>
2024-11-21 18:43:03 -0500

    Add files via upload

---
+++ main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class main {
+
+    public static void main(String[] args) {
+        int opcion =0;
+        while (opcion !=3) {
+            String menu = "MENU DE PROYECTOS: \n"
+            + "1. Ejecutar Punto #2 Gabriel Jaime Cano Oquendo \n"
+            + "2. Ejecutar Punto #9 Salomé Gil Chanci \n"
+            + "3. SALIR \n";
+            
+        }
+    }
+}
